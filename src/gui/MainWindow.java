bd57703ed6102301c4f4ad2f39efd52895e296d6
Michael Ripley <zkxs00@gmail.com>
2014-11-06 03:47:07 -0600

    Re-added boilerplate similar to that that WindowBuilder had created, and display test text.

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -1,11 +1,66 @@
 package gui;
 
-import javax.swing.JFrame;
+import java.awt.*;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 
 public class MainWindow extends JFrame
 {
 	/** WTF IS THIS SHIT??? */
 	private static final long serialVersionUID = 2740437090361841747L;	
+	
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run()
+			{
+				try
+				{
+					JFrame window = new MainWindow();
+					window.setVisible(true);
+				}
+				catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	MainWindow()
+	{
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, -1, -1); // move/resize frame (resizing is done later in pack())
+		
+		JPanel contentPane = new JPanel();
+		
+		contentPane.setBorder(new LineBorder(Color.BLACK, 5));
+		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.X_AXIS));
+		contentPane.setMinimumSize(new Dimension(200, 100));
+		contentPane.setPreferredSize(contentPane.getMinimumSize());
+		
+		setContentPane(contentPane);
+		pack();
+		
+		/* set minimum size based off of contents instead of worrying about
+		 * an arbitrarily dimensioned border
+		 */
+		setMinimumSize(getSize());
+		contentPane.setPreferredSize(new Dimension(800, 600));
+		pack();
+		
+		JLabel label = new JLabel("Hello, World!");
+		label.setFont(label.getFont().deriveFont(64.0f));
+		label.setHorizontalAlignment(JLabel.CENTER);
+		label.setVerticalAlignment(JLabel.CENTER);
+		contentPane.add(Box.createHorizontalGlue());
+		contentPane.add(label);
+		contentPane.add(Box.createHorizontalGlue());
+		//contentPane.add(new JLabel("faq"));
+	}
 	
 	
 }
